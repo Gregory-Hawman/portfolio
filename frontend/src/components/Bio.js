83dbbacb372d232c770d9a99d51cfb98ca2b6874
Gregory Hawman <g.hawman@gmail.com>
2021-04-28 09:36:05 -0700

    css working

--- frontend/src/components/Bio.js
+++ frontend/src/components/Bio.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Element } from 'react-scroll'
 import '../css/bio.css'
+import profile from '../images/profile-img.jpg'
 
 function Bio () {
 
     return (
         <div>
             <Element id='bio-section' name='bio-section'>
-                <div>
-                    <h1>Hello, I am Gregory </h1>
-                    <p>I am a full stack web developer with a passion for coding well crafted easy to use websites and apps.</p>
+                <div className='profile'>
+                    <div className='profile-img'>
+                        <img alt='profile image' src={profile}/>
+                    </div>
+                    <div className='profile-content'>
+                        <h1>Hello, I am Gregory </h1>
+                        <p>I am a full stack web developer with a passion for coding well crafted easy to use websites and apps.</p>
+                    </div>
                 </div>
             </Element>
-
         </div>
-        
-        
     )
 }
 
